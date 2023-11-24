85ae5eb1570b74a6c1168ba5aae22e8794c1c0bb
vanahit1989 <anahit_vardevanyan@ultralabs.io>
2023-11-24 18:59:43 +0400

    print app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useRef} from 'react';
 import './App.css';
+import ReactToPrint from "react-to-print";
+import DocViewer, { DocViewerRenderers } from "@cyntler/react-doc-viewer";
+
+const docs = [
+    {uri: 'https://dev-xuj0ouyv-file-bucket.s3.us-west-2.amazonaws.com/a7bce809-7eb2-4599-bf7c-094ea25e15e1/0fbdde43-eb21-4853-bb8b-d536c15b28ee'}
+];
 
 function App() {
+  const ref = useRef<null | HTMLIFrameElement>(null);
+  const element  = document.getElementById('msdoc-iframe');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ReactToPrint content={() => element} trigger={() => <button>Print</button>} />
+            <DocViewer   documents={docs} pluginRenderers={DocViewerRenderers} />
+
     </div>
   );
 }
